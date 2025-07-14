8adf0260b6e272c1442d928565ef6bd2e6723dc5
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-14 22:55:15 +0530

    Add rectangle class with area and perimeter methods to prac.js

--- prac.js
+++ prac.js
@@ -4,3 +4,18 @@ console.log(data);
 
 const data2 = fs.readFileSync("b.txt", "utf-8");
 console.log(data2);
+
+class rectangle {
+  constructor(length, breadth) {
+    this.length = length;
+    this.breadth = breadth;
+  }
+  area() {
+    return this.length * this.breadth;
+  }
+  perimeter() {
+    return 2 * (this.length + this.breadth);
+  }
+}
+const rect = new rectangle(10, 5);
+console.log("Area of rectangle:", rect.area());
